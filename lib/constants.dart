295341afe4301cc7c61a8a8743e008e1d36734de
Ahmad-Montasser <41066332+Ahmad-Montasser@users.noreply.github.com>
2021-04-27 13:48:24 +0200

    Added a Welcome Screen & an icon

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF623797);
+const kSecondaryColor = Color(0xFFD0B3F8);
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_pal/constants.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_top.png",
+                width: size.width * 0.3,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_bottom.png",
+                width: size.width * 0.2,
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                  ),
+                  Center(
+                    child: Container(
+                      width: size.width * 0.6,
+                      child: RaisedButton(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                        color: kPrimaryColor,
+                        highlightColor: kSecondaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Bungee",
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Center(
+                    child: Container(
+                      width: size.width * 0.6,
+                      child: RaisedButton(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                        color: kPrimaryColor,
+                        highlightColor: kSecondaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Bungee",
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
